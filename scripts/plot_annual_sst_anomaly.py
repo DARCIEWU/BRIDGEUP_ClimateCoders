ec72b558aa18688d205ff52f72bc3afa67f95f68
Darcie <darciew5848@laguardiahs.org>
2020-02-27 17:12:07 -0500

    khfiwuehf

--- scripts/plot_annual_sst_anomaly.py
+++ scripts/plot_annual_sst_anomaly.py
@@ -22,19 +22,32 @@ def main():
 	temp = data.variables['temp'][:]
 	salt = data.variables['salt'][:]
 
-	
+	print(lon.shape) #720
+	print(lat.shape) #330
+	print(time.shape) #12
+	print(depth.shape) #50
+	print(temp.shape) #12, 50, 330, 720
+	print(salt.shape) #12, 2, 330, 720
+
 	condition_depth = depth <= 20
 	temp_sliced = temp[:,condition_depth,:,:]
-	
+	print(temp_sliced.shape)
 
 	
 	temp_mean = temp_sliced.mean()
 	
 
 	temp_anom = temp[:,condition_depth,:,:] - temp_mean
+	print(temp_anom.shape)
 	temp_anom_yr = temp_anom.mean(axis = 0)
-	print(temp_anom_yr.shape(axis = 3))
-	print(temp_anom_yr.mean(axis = 3))
+	print(temp_anom_yr.shape)
+	temp_anom_ = temp_anom_yr.mean(axis = 0)
+	print(temp_anom_.shape)
+
+
+	print(temp_anom_yr.mean)
+	print(temp_anom_yr.shape)
+
 #Execute main script
 if __name__ == '__main__':
 	main()
